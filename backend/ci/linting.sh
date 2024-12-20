6396dd7832f2112c7a24ecbd67b9237d52e37847
Palak Bartek EXT <bartek@man.poznan.pl>
2024-12-20 12:34:44 +0100

    CI files - the same script for hook and ci

--- backend/ci/linting.sh
+++ backend/ci/linting.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Exporting a variable without assigning a value
+
 # Bamboo CI script for linting
 # Note: this script should be run from the root of the git repository
 
@@ -7,29 +9,80 @@ set -e -o pipefail
 echo "Loading modules..."
 
 # Set up environment s
-source backend/ci/configure_env.sh
+BACKEND_ROOT_DIR=$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..")
+source ${BACKEND_ROOT_DIR}/ci/configure_env.sh
 
-set -x
+#set -x
+
+cd ${BACKEND_ROOT_DIR}
+
+
+if [[ "${RUN_MODE_HOOK}" == "true" ]]; then
+    TO_BE_CHECKED=${FILES}
+else
+    TO_BE_CHECKED=ibex
+fi
 
 # Create a venv
 python -m venv venv
 . venv/bin/activate
+echo "PWD: " `pwd`
+# PREPARE THE ENVIRONMENT
+pip install --upgrade ./[linting]
 
-# Install and run linters
-pip install --upgrade ./backend[linting]
-
-# Static code analysis
 rm -rf test-reports
 mkdir -p test-reports
 
+
+
+
+# STATIC CODE ANALYSIS
 # Black: The code formatter
-python -m pytest backend/ibex --black --config-file=backend/pyproject.toml --junitxml=test-reports/black-report.xml
+echo -e "${GREEN}Running black...${NC}"
+python -m pytest  --black --config-file=pyproject.toml --junitxml=test-reports/black-report.xml ${TO_BE_CHECKED}
+#black --check $FILES
+if [ $? -ne 0 ]; then
+    echo -e "${RED}black check failed. Please fix the issues...${NC}"
+    if [[ "${RUN_MODE_HOOK}" == "true" ]]; then
+        exit 1
+    fi
+fi
 
 # isort: a Python utility to sort imports alphabetically
-python -m pytest backend/ibex --isort --config-file=backend/pyproject.toml --junitxml=test-reports/isort-report.xml
-
-# Mypy:  a static type checker for Python
-python -m pytest backend/ibex --mypy --config-file=backend/pyproject.toml --junitxml=test-reports/mypy-report.xml
+echo -e "${GREEN}Running isort...${NC}"
+python -m pytest --isort --config-file=pyproject.toml --junitxml=test-reports/isort-report.xml ${TO_BE_CHECKED}
+# isort --check-only $FILES
+if [ $? -ne 0 ]; then
+    echo -e "${RED}isort check failed. Please fix the issues...${NC}"
+    if [[ "${RUN_MODE_HOOK}" == "true" ]]; then
+        exit 1
+    fi
+fi
 
 # Flake8: linting and style checking
-python -m pytest backend/ibex --flake8 --config-file=backend/pyproject.toml --junitxml=test-reports/flake8-report.xml
+echo -e "${GREEN}Running flake8...${NC}"
+
+python -m pytest --flake8 --config-file=pyproject.toml --junitxml=test-reports/flake8-report.xml ${TO_BE_CHECKED}
+
+#flake8 $FILES
+if [ $? -ne 0 ]; then
+    echo -e "${RED}flake8 check failed. Please fix the issues...${NC}"
+    if [[ "${RUN_MODE_HOOK}" == "true" ]]; then
+        exit 1
+    fi
+fi
+
+# Mypy:  a static type checker for Python
+echo -e "${GREEN}Running mypy...${NC}"
+#mypy $FILES
+python -m pytest --mypy --config-file=pyproject.toml --junitxml=test-reports/mypy-report.xml ${TO_BE_CHECKED}
+if [ $? -ne 0 ]; then
+    echo -e "${RED}mypy check failed. Please fix the issues ...${NC}"
+    if [[ "${RUN_MODE_HOOK}" == "true" ]]; then
+        exit 1
+    fi
+fi
+
+# If all checks pass
+echo -e "${GREEN}All checks passed. Proceeding with commit...${NC}"
+exit 0
--- backend/setup_git-hooks.sh
+++ backend/setup_git-hooks.sh
@@ -1,3 +1,34 @@
 #!/usr/bin/env bash
 
-pre-commit install
+BACKEND_ROOT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+
+PRE_COMMIT_HOOK_PATH="$(realpath "${BACKEND_ROOT_DIR}/../.git/hooks/pre-commit")"
+
+echo ${PRE_COMMIT_HOOK_PATH}
+cat > ${PRE_COMMIT_HOOK_PATH} << SCRIPT_BLOCK
+
+# Custom Git pre-commit hook for Python code quality checks
+BACKEND_ROOT_DIR=${BACKEND_ROOT_DIR}
+
+# Define colors for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No color
+
+# Find staged Python files
+FILES=$(git diff --cached --name-only --diff-filter=ACM | grep '\.py$')
+#FILES=$(git diff --cached --name-only --diff-filter=ACM)
+if [ -z "\$FILES" ]; then
+    echo -e "\${YELLOW}No staged Python files to check.\${NC}"
+    exit 0
+fi
+
+RUN_MODE_HOOK=true
+echo -e "\${YELLOW}Running checks on staged Python files...\${NC}"
+
+source \${BACKEND_ROOT_DIR}/ci/linting.sh
+
+SCRIPT_BLOCK
+
+chmod +x ${PRE_COMMIT_HOOK_PATH}
