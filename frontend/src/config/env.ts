49835ebabab87e0d658acc27dd6a7091583dc59f
Ambasse DJABIRI <adjabiri@epsyl-alcen.com>
2025-01-22 10:19:53 +0100

    feat: set default backend port 8000

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -1,4 +1,4 @@
 export const ENV_VARIABLE = {
-  API_PORT: parseInt(process.env.API_PORT || '3000', 10),
+  API_PORT: parseInt(process.env.API_PORT || '8000', 10),
   API_HOST: process.env.API_HOST || '127.0.0.1',
 };
