10fc0cda16ebaeb9d7e31b2adc18e2a0ca40f943
Ambasse DJABIRI <adjabiri@epsyl-alcen.com>
2025-01-24 16:51:30 +0100

    feat: update handle select accordion

--- frontend/src/renderer/components/tree/TreeLibrariesAccordion.tsx
+++ frontend/src/renderer/components/tree/TreeLibrariesAccordion.tsx
@@ -5,63 +5,26 @@ import { TreeLibrary } from '../../components';
 interface VisualizationTreeProps {
   dataTree: CustomTreeData[];
   height: string;
-  getDataSelected: (data: DataTreeSelected) => void;
+  handleAccordionChange: (value: string) => void;
 }
 
 export const TreeLibrariesAccordion = ({
   dataTree,
   height,
+  handleAccordionChange,
 }: VisualizationTreeProps) => {
-
-  const handleAccordionChange = async (value: string) => {
-    if (value) {
-      /**
-       * Fetch ids children 
-       */
-      try {
-            
-        const selectedItem = dataTree.find((item) => item.name === value);
-
-        console.log("selectedItem", selectedItem);
-
-        const responseNodeInfo= await fetch(
-          `${window.env.API_URL}/ids_info/node_info/?uri=${encodeURIComponent(`${selectedItem.uri}#${selectedItem.name}:${selectedItem.occurrences[0]}`)}`,
-          {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          },
-        );
-
-        if (!responseNodeInfo.ok) {
-          const error = await responseNodeInfo.json();
-          throw new Error(error.detail || 'Failed to fetch IDS data');
-        }
-  
-        const nodeInfos = await responseNodeInfo.json();
-
-        console.log("nodeInfos", nodeInfos);
-
-        
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  };
-
   const items = dataTree.map((item) => (
     <Accordion.Item key={`accodion-${item.name}`} value={item.name}>
       <Accordion.Control>{item.name}</Accordion.Control>
       <Accordion.Panel>
-        <TreeLibrary treeData={item.data} uri={item.uri}/>
+        <TreeLibrary treeData={item.data} uri={item.uri} />
       </Accordion.Panel>
     </Accordion.Item>
   ));
 
   return (
     <ScrollArea h={height}>
-      <Accordion>{items}</Accordion>
+      <Accordion onChange={handleAccordionChange}>{items}</Accordion>
     </ScrollArea>
   );
 };
--- frontend/src/renderer/pages/visualization/VisualizationTree.tsx
+++ frontend/src/renderer/pages/visualization/VisualizationTree.tsx
@@ -14,32 +14,85 @@ export const VisualizationTree = ({ height }: VisualizationTreeProps) => {
 
   useEffect(() => {
     if (active && active.dataIDS) {
-      const newCustomDataTree: CustomTreeData[] = active.dataIDS.map((ids) => ({
-        name: ids.name,
-        uri: ids.uri,
-        occurrences: ids.occurrences,
-        data: [
-          {
-            label: ids.name,
-            value: ids.name,
-            children: [], // Les enfants seront ajoutés dynamiquement
-          },
-        ],
-      }));
+      const newCustomDataTree: CustomTreeData[] = [];
+
+      // active.dataIDS.map((ids) => ({
+      //   name: ids.name,
+      //   uri: ids.uri,
+      //   occurrences: ids.occurrences,
+      //   data: [
+      //     {
+      //       label: ids.name,
+      //       value: ids.name,
+      //       children: [], // Les enfants seront ajoutés dynamiquement
+      //     },
+      //   ],
+      // }));
       setCustomDataTree(newCustomDataTree);
     }
   }, [active]);
 
-  const getDataSelected = (data: DataTreeSelected) => {
-    console.log(data);
+  const handleAccordionChange = async (value: string) => {
+    if (value) {
+      /**
+       * Fetch ids children
+       */
+      try {
+        const selectedDataTree = customDataTree.find((item) => item.name === value);
+
+        const responseNodeInfo = await fetch(
+          `${window.env.API_URL}/ids_info/node_info/?uri=${encodeURIComponent(`${selectedDataTree.uri}#${selectedDataTree.name}:${selectedDataTree.occurrences[0]}`)}`,
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          },
+        );
+
+        if (!responseNodeInfo.ok) {
+          const error = await responseNodeInfo.json();
+          throw new Error(error.detail || 'Failed to fetch IDS data');
+        }
+
+        const nodeInfos = await responseNodeInfo.json();
+
+        if (nodeInfos.children.lenght > 0) {
+          const newChildren: TreeNodeData[] = [];
+          nodeInfos.children.map((child: any) => {
+            newChildren.push({
+              label: child.name,
+              value: child.name,
+              children: [],
+            });
+          });
+          selectedDataTree.data.push(...newChildren);
+
+          const updatedCustomDataTree = customDataTree.map((item) => {
+            if (item.name === value) {
+              return {
+                ...item,
+                data: selectedDataTree.data,
+              };
+            }
+            return item;
+          });
+
+          setCustomDataTree(updatedCustomDataTree);
+
+        }
+        console.log('nodeInfos', nodeInfos);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   };
 
   return (
     <TreeLibrariesAccordion
       dataTree={customDataTree}
-      getDataSelected={getDataSelected}
       height={height}
+      handleAccordionChange={handleAccordionChange}
     />
   );
 };
-
