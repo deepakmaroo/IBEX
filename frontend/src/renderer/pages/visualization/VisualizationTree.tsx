7ed6d050b90ab75feee7d279946489060d7033be
Ambasse DJABIRI <adjabiri@epsyl-alcen.com>
2025-01-28 16:09:00 +0100

    clean comment code

--- frontend/src/renderer/pages/visualization/VisualizationTree.tsx
+++ frontend/src/renderer/pages/visualization/VisualizationTree.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect } from 'react';
 import { TreeLibrariesAccordion } from '../../components';
 import { useIbexStore } from '../../stores';
 import { Configuration, CustomTreeData } from 'src/renderer/types';
@@ -11,7 +11,9 @@ interface VisualizationTreeProps {
 export const VisualizationTree = ({ height }: VisualizationTreeProps) => {
   const { active, setActive, updatedConfiguration } = useIbexStore();
 
-  
+  /**
+   * Handle dataIDS change
+   */
   useEffect(() => {
     if (active && active.dataIDS) {
       const newCustomDataTree: CustomTreeData[] = active.dataIDS.map((ids) => ({
@@ -29,16 +31,88 @@ export const VisualizationTree = ({ height }: VisualizationTreeProps) => {
     }
   }, [active.dataIDS]);
 
-  const handleAccordionChange =  useCallback(async(value: string) => {
-    if (value) {
-      /**
-       * Fetch ids children
-       */
-      try {
-        const selectedDataTree = active.customDataTree.find((item) => item.name === value);
+  /**
+   * Handle accordion change
+   * @param value
+   * @returns
+   */
+  const handleAccordionChange = useCallback(
+    async (value: string) => {
+      if (value) {
+        try {
+          const selectedDataTree = active.customDataTree.find(
+            (item) => item.name === value,
+          );
 
+          const responseNodeInfo = await fetch(
+            `${window.env.API_URL}/ids_info/node_info/?uri=${encodeURIComponent(`${selectedDataTree.uri}#${selectedDataTree.name}:${selectedDataTree.occurrences[0]}`)}`,
+            {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            },
+          );
+
+          if (!responseNodeInfo.ok) {
+            const error = await responseNodeInfo.json();
+            throw new Error(error.detail || 'Failed to fetch IDS data');
+          }
+
+          const nodeInfos = await responseNodeInfo.json();
+          console.log('nodeInfos', nodeInfos);
+          console.log('nodeInfos children', nodeInfos.children);
+
+          if (nodeInfos.children.length > 0) {
+            const newChildren: TreeNodeData[] = [];
+            nodeInfos.children.map((child: any) => {
+              newChildren.push({
+                label: child.name,
+                value: `#${value}:0/${child.name}`,
+                children: [],
+              });
+            });
+
+            // Add children to the selectedDataTree if not exists
+            if (!selectedDataTree.data.length) {
+              const updatedCustomDataTree = active.customDataTree.map(
+                (item) => {
+                  if (item.name === value) {
+                    return {
+                      ...item,
+                      data: newChildren,
+                    };
+                  }
+                  return item;
+                },
+              );
+
+              const updatedActive: Configuration = {
+                ...active,
+                customDataTree: updatedCustomDataTree,
+              };
+              updatedConfiguration(updatedActive);
+              setActive(updatedActive.name);
+            }
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    },
+    [active],
+  );
+
+  /**
+   * Fetch children node infos
+   * @param uri
+   * @param nodeValue
+   */
+  const fetchChildrenNodeInfos = useCallback(
+    async (uri: string, nodeValue: string) => {
+      try {
         const responseNodeInfo = await fetch(
-          `${window.env.API_URL}/ids_info/node_info/?uri=${encodeURIComponent(`${selectedDataTree.uri}#${selectedDataTree.name}:${selectedDataTree.occurrences[0]}`)}`,
+          `${window.env.API_URL}/ids_info/node_info/?uri=${encodeURIComponent(`${uri}/${nodeValue}`)}`,
           {
             method: 'GET',
             headers: {
@@ -53,117 +127,66 @@ export const VisualizationTree = ({ height }: VisualizationTreeProps) => {
         }
 
         const nodeInfos = await responseNodeInfo.json();
-        console.log('nodeInfos', nodeInfos);
-        console.log('nodeInfos children', nodeInfos.children);
 
-        if (nodeInfos.children.length > 0) {
-          const newChildren: TreeNodeData[] = [];
-          nodeInfos.children.map((child: any) => {
-            newChildren.push({
-              label: child.name,
-              value: `#${value}:0/${child.name}`,
-              children: [],
-            });
+        /**
+         * Update the children of the node
+         * @param nodes
+         * @param nodeValueToUpdate
+         * @returns
+         */
+        const updateNodeChildren = (
+          nodes: TreeNodeData[],
+          nodeValueToUpdate: string,
+        ): TreeNodeData[] => {
+          return nodes.map((node) => {
+            if (node.value === nodeValueToUpdate) {
+              const newChildren: TreeNodeData[] = nodeInfos.children.map(
+                (child: any) => ({
+                  label: child.name,
+                  value: `${nodeValue}/${child.name}`,
+                }),
+              );
+
+              return {
+                ...node,
+                children: newChildren,
+              };
+            }
+
+            // If the node has children, update them
+            if (node.children && node.children.length > 0) {
+              return {
+                ...node,
+                children: updateNodeChildren(node.children, nodeValueToUpdate),
+              };
+            }
+
+            return node;
           });
-          
-          // Add children to the selectedDataTree if not exists
-          if (!selectedDataTree.data.length) {
-            const updatedCustomDataTree = active.customDataTree.map((item) => {
-              if (item.name === value) {
-                return {
-                  ...item,
-                  data: newChildren,
-                };
-              }
-              return item;
-            });
-            
-            const updatedActive: Configuration = {
-              ...active,
-              customDataTree: updatedCustomDataTree,
+        };
+
+        const updatedCustomDataTree = active.customDataTree.map((item) => {
+          if (item.uri === uri) {
+            return {
+              ...item,
+              data: updateNodeChildren(item.data, nodeValue),
             };
-            updatedConfiguration(updatedActive);
-            setActive(updatedActive.name);
           }
-        }
+          return item;
+        });
+        
+        const updatedActive: Configuration = {
+          ...active,
+          customDataTree: updatedCustomDataTree,
+        };
+        updatedConfiguration(updatedActive);
+        setActive(updatedActive.name);
       } catch (error) {
         console.error(error);
       }
-    }
-  }, [active]);
-
-  const fetchChildrenNodeInfos = useCallback(async (uri: string, nodeValue: string) => {
-    try {
-      const responseNodeInfo = await fetch(
-        `${window.env.API_URL}/ids_info/node_info/?uri=${encodeURIComponent(`${uri}/${nodeValue}`)}`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-  
-      if (!responseNodeInfo.ok) {
-        const error = await responseNodeInfo.json();
-        throw new Error(error.detail || 'Failed to fetch IDS data');
-      }
-  
-      const nodeInfos = await responseNodeInfo.json();
-  
-      // Fonction récursive pour mettre à jour un nœud spécifique
-      const updateNodeChildren = (nodes: TreeNodeData[], nodeValueToUpdate: string): TreeNodeData[] => {
-        return nodes.map((node) => {
-          if (node.value === nodeValueToUpdate) {
-            // Met à jour les enfants du nœud trouvé
-            const newChildren: TreeNodeData[] = nodeInfos.children.map((child: any) => ({
-              label: child.name,
-              value: `${nodeValue}/${child.name}`,
-            }));
-  
-            return {
-              ...node,
-              children: newChildren,
-            };
-          }
-  
-          // Si le nœud a des enfants, continuer à chercher récursivement
-          if (node.children && node.children.length > 0) {
-            return {
-              ...node,
-              children: updateNodeChildren(node.children, nodeValueToUpdate),
-            };
-          }
-  
-          return node;
-        });
-      };
-  
-      // Met à jour l'arbre personnalisé
-      const updatedCustomDataTree = active.customDataTree.map((item) => {
-        if (item.uri === uri) {
-          return {
-            ...item,
-            data: updateNodeChildren(item.data, nodeValue),
-          };
-        }
-        return item;
-      });
-  
-      console.log('updatedCustomDataTree', updatedCustomDataTree);
-  
-      // Si nécessaire, mettre à jour l'état global
-      const updatedActive: Configuration = {
-        ...active,
-        customDataTree: updatedCustomDataTree,
-      };
-      updatedConfiguration(updatedActive);
-      setActive(updatedActive.name);
-      console.log("active", active)
-    } catch (error) {
-      console.error(error);
-    }
-  }, [active]);
+    },
+    [active],
+  );
 
   return (
     <TreeLibrariesAccordion
