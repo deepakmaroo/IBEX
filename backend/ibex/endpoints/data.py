74ac6bec8a422413e4cdf589d195ca4d5fc5b8a6
wasikj <jwasik@man.poznan.pl>
2025-03-06 08:40:15 +0100

    Add test. Fix field_value endpoint.

--- backend/ibex/endpoints/data.py
+++ backend/ibex/endpoints/data.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, Query  # type: ignore
+from fastapi import APIRouter, Query, HTTPException  # type: ignore
 
 from ibex.core import ibex_service
 
@@ -10,7 +10,7 @@ router = APIRouter()
 @router.get("/data/field_value/")
 @ibex_service.measure_execution_time
 async def field_value(uri: str, range: List[int] = Query(None)) -> dict:
-    # try:
-    return ibex_service.get_data(uri, range)
-    # except Exception as e:
-    #    raise HTTPException(status_code=404, detail=f"{e}")
+    try:
+        return ibex_service.get_data(uri, range)
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=f"{e}")
