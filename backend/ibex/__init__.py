63262b6717ff94346a26e2c19c25119c9a2b6ec6
wasikj <jwasik@man.poznan.pl>
2025-03-19 11:54:15 +0100

    Finish adding exception handlers

--- backend/ibex/__init__.py
+++ backend/ibex/__init__.py
@@ -1,3 +1,10 @@
+import logging
+from ibex.setup_logging import connect_formatter
+
 from . import _version
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+connect_formatter(logger)
 
 __version__ = _version.get_versions()["version"]
--- backend/ibex/main.py
+++ backend/ibex/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Request  # type: ignore
-from fastapi.responses import JSONResponse  # type: ignore
+from fastapi import FastAPI  # type: ignore
 import logging  # type: ignore
 
 from ibex.endpoints.data import router as data_router
 from ibex.endpoints.data_entry import router as data_entry_router
 from ibex.endpoints.ids_info import router as ids_info_router
+
+from .exception_handlers import general_exception_handler, value_error_handler, key_error_handler, runtime_error_handler
 
 logger = logging.getLogger(__name__)
 
@@ -14,16 +15,7 @@ app.include_router(data_entry_router)
 app.include_router(ids_info_router)
 app.include_router(data_router)
 
-
-@app.exception_handler(Exception)
-async def custom_exception_handler(request: Request, exc: Exception):
-    """
-    :param request: Received request
-    :param exc: Raised exception
-    :return: JSONResponse to be sent to frontend
-    """
-    code = 404
-    if hasattr(exc, "code"):
-        code = exc.code
-    logger.error(f"{exc}")
-    return JSONResponse(status_code=code, content={"message": repr(exc)})
+app.add_exception_handler(Exception, general_exception_handler)
+app.add_exception_handler(ValueError, value_error_handler)
+app.add_exception_handler(KeyError, key_error_handler)
+app.add_exception_handler(RuntimeError, runtime_error_handler)
