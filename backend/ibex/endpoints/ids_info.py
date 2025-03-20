5db837c5c03430a4c9fd18948b42d98d029d3716
wasikj <jwasik@man.poznan.pl>
2025-03-20 12:40:10 +0100

    Fix failing test

--- backend/ibex/endpoints/ids_info.py
+++ backend/ibex/endpoints/ids_info.py
@@ -7,14 +7,14 @@ router = APIRouter()
 
 @router.get("/ids_info/node_info/")
 @ibex_service.measure_execution_time
-async def node_info(uri: str) -> dict:
-    return ibex_service.get_node_info(uri)
+async def node_info(uri: str, show_error_bars: bool = False) -> dict:
+    return ibex_service.get_node_info(uri, show_error_bars)
 
 
 @router.get("/ids_info/find_paths/")
 @ibex_service.measure_execution_time
-async def find_field(uri: str, searched_node: str) -> dict:
-    return ibex_service.find_paths(uri, searched_node)
+async def find_field(uri: str, searched_node: str, show_error_bars: bool = False) -> dict:
+    return ibex_service.find_paths(uri, searched_node, show_error_bars)
 
 
 @router.get("/ids_info/array_summary/")
--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -5,7 +5,6 @@ from fastapi.testclient import TestClient
 
 from ibex.main import app
 
-# app.add_exception_handler(Exception, custom_exception_handler)
 pytest.test_client = TestClient(app)
 
 
