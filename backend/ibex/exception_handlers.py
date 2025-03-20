9a3caa7ed89ebc6394802352f52712ef7647287a
wasikj <jwasik@man.poznan.pl>
2025-03-20 11:21:02 +0100

    Add exception code extraction

--- backend/ibex/exception_handlers.py
+++ backend/ibex/exception_handlers.py
@@ -7,6 +7,9 @@ logger = logging.getLogger(__name__)
 
 
 async def general_exception_handler(request: Request, exc: Exception):
-    code = 404
+    try:
+        code = exc.code
+    except AttributeError:
+        code = 404
     logger.error(traceback.format_exc())
     return JSONResponse(status_code=code, content={"message": str(exc)})
