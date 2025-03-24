111b083e0ccb0e77d11ffb7b3edc7d68a3523bd8
Ambasse DJABIRI <adjabiri@epsyl-alcen.com>
2025-03-24 16:05:44 +0100

    feat: install react grid component

--- frontend/src/renderer/index.tsx
+++ frontend/src/renderer/index.tsx
@@ -4,6 +4,10 @@ import '@mantine/notifications/styles.css';
 import './index.css';
 import { App } from './App';
 
+// Import react-grid-layout styles
+import '../../node_modules/react-grid-layout/css/styles.css';
+import '../../node_modules/react-resizable/css/styles.css';
+
 // Create a dedicated container element for the app
 const container = document.createElement('div');
 container.id = 'root';
--- frontend/src/renderer/pages/visualization/VisualizationPlot.tsx
+++ frontend/src/renderer/pages/visualization/VisualizationPlot.tsx
@@ -1,20 +1,29 @@
 import { Stack, Text } from '@mantine/core';
-import { SimplePlotly } from '../../components';
 import { useIbexStore } from '../../stores';
+import GridLayout from 'react-grid-layout';
+import { SimplePlotly } from 'src/renderer/components';
 
 export const VisualizationPlot = () => {
   const { active } = useIbexStore();
 
   return active.dataPlot.length > 0 ? (
     <>
-      {active.dataPlot.map((plotData, index) => (
-        <SimplePlotly
-          key={index}
-          title={plotData.title}
-          yAxisName={plotData.yAxisName}
-          data={plotData.plot}
-        />
-      ))}
+      <GridLayout
+        cols={12}
+        rowHeight={30}
+        width={1850}
+        autoSize={true}
+        // onLayoutChange={(layout) => updateData(layout)}
+      >
+        {active.dataPlot.map((plotData, index) => (
+          <SimplePlotly
+            key={index}
+            title={plotData.title}
+            yAxisName={plotData.yAxisName}
+            data={plotData.plot}
+          />
+        ))}
+      </GridLayout>
     </>
   ) : (
     <Stack h="100%" align="center" w="100%" justify="center">
