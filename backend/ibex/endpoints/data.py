cbc9d1156e8f9a313ff5f991ade5274fc611d419
wasikj <jwasik@man.poznan.pl>
2025-05-05 11:57:36 +0200

    add field_value docstring

--- backend/ibex/endpoints/data.py
+++ backend/ibex/endpoints/data.py
@@ -12,6 +12,9 @@ router = APIRouter()
 @router.get("/data/field_value/")
 @ibex_service.measure_execution_time
 async def field_value(uri: str, range: List[int] = Query(None)) -> dict:
+    """
+    IBEX endpoint. Returns json {"value":<data_extracted_from_pulsefile>}
+    """
     return ibex_service.get_data(uri, range)
 
 
