f8c3db183e966b94464f76cfa456022ca3d0cddc
Ambasse DJABIRI <adjabiri@epsyl-alcen.com>
2025-05-16 15:12:05 +0200

    fix: height component

--- frontend/src/renderer/pages/visualization/VisualizationTree.tsx
+++ frontend/src/renderer/pages/visualization/VisualizationTree.tsx
@@ -520,7 +520,7 @@ export const VisualizationTree = ({
               <TreeLibrariesAccordion
                 defaultValue={uriSelected?.uri}
                 customDataTree={active.customDataTree}
-                height={`calc(${height} - 115px)`}
+                height={`calc(${height} - 125px)`}
                 checkedNodes={active.checkedNodeURI || []}
                 handleAccordionChange={handleAccordionChange}
                 handleSelectChildren={handleSelectChildren}
