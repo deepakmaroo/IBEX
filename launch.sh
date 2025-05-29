f73bb3ae25fe11b7f1aa0ef69cc2bd3f353b4843
wasikj <jwasik@man.poznan.pl>
2025-05-29 13:00:40 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -24,6 +24,29 @@ cd "$SCRIPT_DIR"
 
 echo "5. Installing frontend dependencies..."
 cd "$SCRIPT_DIR/frontend"
+
+# Search for 2 open ports, one for webpack renderer and one for webpack logger
+port=49152
+found_ports=()
+
+while [ ${#found_ports[@]} -lt 2 ]; do
+  if [ -n "$(ss -tan4H "sport = $port")" ]; then
+    found_ports+=($port)
+  fi
+  port=$((port+1))
+done
+
+echo "Setting \$WEBPACK_RENDERER PORT = ${found_ports[0]}"
+echo "Setting \$WEBPACK_LOGGER PORT = ${found_ports[1]}"
+
+cat > ".env" <<EOF
+# Webpack renderer port
+WEBPACK_PORT=${found_ports[0]}
+
+# Webpack logger port
+LOGGER_PORT=${found_ports[1]}
+EOF
+
 npm install
 
 echo "6. Launch frontend server..."
