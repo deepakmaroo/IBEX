caf91c1b8a7478d83bf0d7ea1d5db7340d44549f
wasikj <jwasik@man.poznan.pl>
2025-06-09 10:12:55 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -28,18 +28,14 @@ cd "$SCRIPT_DIR/frontend"
 # Search for 2 open ports, one for webpack renderer and one for webpack logger
 # The range 49152–65535 contains dynamic or private ports.
 # This range is used for private or customized services, for temporary purposes, and for automatic allocation of ephemeral ports.
-port=49152
 found_ports=()
 
-while [ ${#found_ports[@]} -lt 2 ]; do
-  if [ -z "$(ss -tan4H "sport = $port")" ]; then
-    found_ports+=($port)
-  fi
-  port=$((port+1))
-done
+# https://unix.stackexchange.com/questions/55913/whats-the-easiest-way-to-find-an-unused-local-port
+found_ports[0]=`comm -23 <(seq 49152 65535 | sort) <(ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n 1`
+found_ports[1]=`comm -23 <(seq 49152 65535 | sort) <(ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n 1`
 
-echo "Setting \$WEBPACK_RENDERER PORT = ${found_ports[0]}"
-echo "Setting \$WEBPACK_LOGGER PORT = ${found_ports[1]}"
+echo "Setting WEBPACK_RENDERER PORT = ${found_ports[0]}"
+echo "Setting WEBPACK_LOGGER PORT = ${found_ports[1]}"
 
 cat > ".env" <<EOF
 # Webpack renderer port
