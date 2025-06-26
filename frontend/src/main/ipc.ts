9248146ee9c625c9bba685159c272c30a81de47d
Ambasse DJABIRI <adjabiri@epsyl-alcen.com>
2025-06-26 13:37:37 +0200

    test: add lint error to test

--- frontend/src/main/ipc.ts
+++ frontend/src/main/ipc.ts
@@ -2,7 +2,7 @@ import { ipcMain, dialog, BrowserWindow } from 'electron';
 import * as fs from 'fs';
 import { getConfigSync } from '../config';
 import { ConfigurationState } from 'src/renderer/types';
-
+import { PlotCoordinatesResponse } from 'src/renderer/types';
 export default {
   initialize() {
     const config = getConfigSync();
