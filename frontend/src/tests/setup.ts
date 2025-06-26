d11a18a1361a928ce00fb19ee141e5c34c16a175
Ambasse DJABIRI <adjabiri@epsyl-alcen.com>
2025-06-26 15:41:32 +0200

    test: fix error lint

--- frontend/src/tests/setup.ts
+++ frontend/src/tests/setup.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import * as chrome from 'selenium-webdriver/chrome';
 import { Builder, WebDriver } from 'selenium-webdriver';
 import { ConfigurationState } from 'src/renderer/types';
-import { ipcMain, dialog } from 'electron';
+
 let driver: WebDriver;
 let electron: ChildProcessWithoutNullStreams;
 /**
