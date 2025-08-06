93120abc98a277e78d93d363bd59c3955da9d7dc
Ambasse DJABIRI <adjabiri@epsyl-alcen.com>
2025-08-06 10:26:26 +0200

    fix: merge conflit unresolved

--- frontend/src/renderer/types/plot.ts
+++ frontend/src/renderer/types/plot.ts
@@ -1,20 +1,6 @@
 import { Data } from 'plotly.js';
 import { Layout } from 'react-grid-layout';
 
-<<<<<<< HEAD
-=======
-export interface SimplePlotlyProps {
-  title: string;
-  xAxis: Axis;
-  yAxis: Axis;
-  y2Axis?: Axis;
-  data: DataPlotly[];
-  width: number;
-  height: number;
-  isStatic: boolean;
-}
-
->>>>>>> feature/switch-x-axis
 export interface Axis {
   name: string;
   unit: string;
