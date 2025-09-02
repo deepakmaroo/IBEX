6a4ceaff26b56e43b379f5453059f607cb11f727
Maël Fiaudrin <mfiaudrin@epsyl-alcen.com>
2025-09-02 11:14:27 +0200

    feat: show by default heatmap when 2D case

--- frontend/src/renderer/components/grid/GridLayoutPlot.tsx
+++ frontend/src/renderer/components/grid/GridLayoutPlot.tsx
@@ -61,6 +61,10 @@ export const GridLayoutPlot = ({
     }
   }, [data.plot]);
 
+  useLayoutEffect(() => {
+    setIs3DView(data.coordinates.length === 3);
+  }, []);
+
   /**
    * Update the width of the slider when the grid is resized
    */
@@ -136,70 +140,21 @@ export const GridLayoutPlot = ({
     [active],
   );
 
-  const heatmapLogo = 
-    (
-      <svg width="50" height="50" viewBox="0 0 50 50">
-        <rect x="0" y="0" width="15" height="15" fill="#440154" />
-        <rect
-          x="17"
-          y="0"
-          width="15"
-          height="15"
-          fill="#31688e"
-        />
-        <rect
-          x="34"
-          y="0"
-          width="15"
-          height="15"
-          fill="#35b779"
-        />
+  const heatmapLogo = (
+    <svg width="50" height="50" viewBox="0 0 50 50">
+      <rect x="0" y="0" width="15" height="15" fill="#440154" />
+      <rect x="17" y="0" width="15" height="15" fill="#31688e" />
+      <rect x="34" y="0" width="15" height="15" fill="#35b779" />
 
-        <rect
-          x="0"
-          y="17"
-          width="15"
-          height="15"
-          fill="#fde725"
-        />
-        <rect
-          x="17"
-          y="17"
-          width="15"
-          height="15"
-          fill="#440154"
-        />
-        <rect
-          x="34"
-          y="17"
-          width="15"
-          height="15"
-          fill="#31688e"
-        />
+      <rect x="0" y="17" width="15" height="15" fill="#fde725" />
+      <rect x="17" y="17" width="15" height="15" fill="#440154" />
+      <rect x="34" y="17" width="15" height="15" fill="#31688e" />
 
-        <rect
-          x="0"
-          y="34"
-          width="15"
-          height="15"
-          fill="#35b779"
-        />
-        <rect
-          x="17"
-          y="34"
-          width="15"
-          height="15"
-          fill="#fde725"
-        />
-        <rect
-          x="34"
-          y="34"
-          width="15"
-          height="15"
-          fill="#440154"
-        />
-      </svg>
-    );
+      <rect x="0" y="34" width="15" height="15" fill="#35b779" />
+      <rect x="17" y="34" width="15" height="15" fill="#fde725" />
+      <rect x="34" y="34" width="15" height="15" fill="#440154" />
+    </svg>
+  );
 
   return (
     <Container fluid w={widthGrid} p={0}>
@@ -225,21 +180,18 @@ export const GridLayoutPlot = ({
           {hovered || data.isEditing ? (
             <Group pos="absolute" right={'1rem'} top={5} grow>
               {/* 3D button display */}
-              <Tooltip label="Toggle 1D/Heatmap view">
-                <ActionIcon
-                  variant="filled"
-                  aria-label="Toggle 1D/Heatmap view"
-                  onClick={() => setIs3DView((prev) => !prev)}
-                  className={classes.actionButton}
-                  disabled={data.coordinates.length !== 3} //Only enable if there are 3 coordinates - corresponding to 3D data
-                >
-                  {is3DView ? (
-                    <Text fw="bold">1D</Text>
-                  ) : (
-                    heatmapLogo
-                  )}
-                </ActionIcon>
-              </Tooltip>
+              {data.coordinates.length === 3 && ( //Only show if there are 3 coordinates - corresponding to 3D data
+                <Tooltip label="Toggle 1D/Heatmap view">
+                  <ActionIcon
+                    variant="filled"
+                    aria-label="Toggle 1D/Heatmap view"
+                    onClick={() => setIs3DView((prev) => !prev)}
+                    className={classes.actionButton}
+                  >
+                    {is3DView ? <Text fw="bold">1D</Text> : heatmapLogo}
+                  </ActionIcon>
+                </Tooltip>
+              )}
               {/* Metadata component button */}
               <Tooltip label="Inspect metadatas information">
                 <ActionIcon
